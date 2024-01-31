03ad7157eeae332c8021ed656adba3b57ca6c141
kunal1003 <gavalikunal321@gmail.com>
2024-02-01 00:47:42 +0530

    changes of hss-111 added

--- demo/src/main/java/com/example/demo/controller/Car.java
+++ demo/src/main/java/com/example/demo/controller/Car.java
@@ -5,6 +5,14 @@ public class Car {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      
+		
+		
+	}
+	
+	public void m1() {
+		
+		// changes regarding hss-111
+		
 	}
 
 }
